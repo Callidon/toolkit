a26b24b81bd2fecf37dab0387a39e241c8cd1583
Thomas Minier <tminier01@gmail.com>
2016-06-03 11:23:16 +0200

    use only bytes manipulation in bencode lib

--- dfs/bencode/bencode.go
+++ dfs/bencode/bencode.go
@@ -3,7 +3,6 @@ package bencode
 import (
 	"bytes"
 	"strconv"
-	"strings"
 )
 
 // Serialize a string using Bencode standard
@@ -27,10 +26,9 @@ func SerializeList(list []string) []byte {
 // Deserialize a string serialized using Bencode standard
 // Reference : https://en.wikipedia.org/wiki/Bencode
 func DeserializeStr(bencode []byte) (string, int) {
-	str := string(bencode)
-	strLength := strings.Split(str, ":")[0]
-	length, _ := strconv.Atoi(strLength)
-	return str[len(strLength)+1:], length
+    strLength := bytes.SplitN(bencode, []byte(":"), 2)[0]
+    length, _ := strconv.Atoi(string(strLength))
+    return string(bencode[len(strLength)+1:]), length
 }
 
 // Deserialize a list of strings serialized using Bencode standard
@@ -38,10 +36,9 @@ func DeserializeStr(bencode []byte) (string, int) {
 func DeserializeList(bencode []byte) ([]string, int) {
 	list := make([]string, 0)
 	ind := 1
-	str := string(bencode)
 	for ind < len(bencode)-1 {
-		strLength := strings.Split(str[ind:], ":")[0]
-		length, _ := strconv.Atoi(strLength)
+		strLength := bytes.SplitN(bencode[ind:], []byte(":"), 2)[0]
+        length, _ := strconv.Atoi(string(strLength))
 		elt, _ := DeserializeStr(bencode[ind : ind+length+1])
 		list = append(list, elt)
 		ind += length + 1 + len(strLength)
--- dfs/bencode/bencode_test.go
+++ dfs/bencode/bencode_test.go
@@ -5,17 +5,17 @@ import (
 	"testing"
 )
 
-func TestSerializeStr(t *testing.T) {
-	var datas = []string{
-		"a",
-		"16",
-		"-125",
-		"hello",
-		"lot of spaces",
-		",;:!?./§ù*%µ<>&é(-è_çà)=~#{[|`\\^@]}",
-	}
+var StrDatas = []string{
+    "a",
+    "16",
+    "-125",
+    "hello",
+    "lot of spaces",
+    ",;:!?./§ù*%µ<>&é(-è_çà)=~#{[|`\\^@]}",
+}
 
-	for _, data := range datas {
+func TestSerializeStr(t *testing.T) {
+	for _, data := range StrDatas {
 		serial := SerializeStr(data)
 		dserial, length := DeserializeStr(serial)
 		if dserial != data {
@@ -29,20 +29,13 @@ func TestSerializeStr(t *testing.T) {
 }
 
 func TestSerializeList(t *testing.T) {
-	var datas = [][]string{
-		[]string{"a", "hello", "lot of spaces", ",;:!?./§ù*%µ<>&é(-è_çà)=~#{[|`\\^@]}"},
-		[]string{"1", "-15"},
-	}
-	for _, data := range datas {
-		serial := SerializeList(data)
-		dserial, length := DeserializeList(serial)
-		if reflect.DeepEqual(dserial, data) {
-			t.Error("Error : expected ", data, " from deserialization, got ", dserial)
-		}
-
-		if length != len(data) {
-			t.Error("Error : expected length = ", len(data), ", got ", len(dserial))
-		}
+	serial := SerializeList(StrDatas)
+	dserial, length := DeserializeList(serial)
+	if reflect.DeepEqual(dserial, StrDatas) {
+		t.Error("Error : expected ", StrDatas, " from deserialization, got ", dserial)
 	}
 
+	if length != len(StrDatas) {
+		t.Error("Error : expected length = ", len(StrDatas), ", got ", len(dserial))
+	}
 }
