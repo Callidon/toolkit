701a5860f18fdcceffeadb730e4356af60d53459
Thomas Minier <tminier01@gmail.com>
2016-07-27 14:28:14 +0200

    implement merge sort algorithm in rust

--- rust-lab/src/main.rs
+++ rust-lab/src/main.rs
@@ -1,37 +1,15 @@
+// Author : Thomas Minier
 extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+mod guessing;
+mod mergesort;
 
 fn main() {
-    println!("Guess the number");
+    // Guessing game
+    //guessing::guessing();
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is {}", secret_number);
-
-    loop {
-        println!("Please input your guess");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_)  => continue,
-        };
-
-        println!("You guessed {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less     => println!("Too small !"),
-            Ordering::Greater  => println!("To big !"),
-            Ordering::Equal    => {
-                println!("You win !");
-                break;
-            }
-        }
-    }
+    // Merge sort
+    let v = vec!(5,1,4,3,2,6);
+    let w = mergesort::merge_sort(&v);
+    println!("{:?}", w);
 }
